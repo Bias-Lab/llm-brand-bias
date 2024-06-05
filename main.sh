ed69125967379a0cb9034d8e9e13a4d07a944ba2
hieuminh65 <hieuminhnguyen@usf.edu>
2024-06-05 12:11:05 -0400

    add llama3 data

--- main.sh
+++ main.sh
@@ -1,19 +1,42 @@
 #!/bin/bash
 
 # Llama3
-ollama run llama3
-/bye
+echo "/bye" | ollama run llama3
 export MODEL=llama3
 nohup python3 main.py --mode=local &
 echo "Running llama3 done"
 
-# Get the PID of the process we just started
 pid=$!
 
-# Wait for the python script to finish
 wait $pid
 
-# Commit the changes and push to github
 git add .
 git commit -m "add llama3 data"
+
+# Gemma
+echo "/bye" | ollama run gemma
+export MODEL=gemma
+nohup python3 main.py --mode=local &
+echo "Running gemma done"
+
+pid=$!
+
+wait $pid
+
+git add .
+git commit -m "add gemma data"
+
+# Mistral
+echo "/bye" | ollama run mistral
+export MODEL=mistral
+nohup python3 main.py --mode=local &
+echo "Running mistral done"
+
+pid=$!
+wait $pid
+
+git add .
+git commit -m "add mistral data"
+
+# Commit the changes and push to github
 git push origin ssh
